9e01cf26e6d7aab6a280f13d1b9557067f10d96b
NishinoJunji <nishinojunji@gmail.com>
2024-06-20 13:35:59 +0900

    Update sample.js

--- sample.js
+++ sample.js
@@ -1,8 +1,16 @@
 function setup() {
   createCanvas(400, 400);
+  background(220);
 }
 
+
 function draw() {
-    
-  background(220);
+  // background(220);
+ 
+  let x1 = random(0, 400);
+  let y1 = random(0, 400);
+  let x2 = random(0, 400);
+  let y2 = random(0, 400);
+  
+  line(x1, y1, x2, y2);
 }
